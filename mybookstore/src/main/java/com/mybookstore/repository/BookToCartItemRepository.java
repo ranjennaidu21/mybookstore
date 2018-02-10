dbc91b2dfdb7bb05701f3e36b287cfea87cd5fbe
Anonymous <Anonymous@Anonymous-PC>
2018-02-10 18:20:42 +0800

    Bookstore - Update Qyt and Delete function in ShoppingCart

--- mybookstore/src/main/java/com/mybookstore/repository/BookToCartItemRepository.java
+++ mybookstore/src/main/java/com/mybookstore/repository/BookToCartItemRepository.java
@@ -1,9 +1,12 @@
 package com.mybookstore.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mybookstore.domain.BookToCartItem;
+import com.mybookstore.domain.CartItem;
 
+@Transactional
 public interface BookToCartItemRepository extends CrudRepository<BookToCartItem, Long> {
-
+	void deleteByCartItem(CartItem cartItem);
 }
